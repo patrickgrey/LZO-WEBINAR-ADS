4e59393f64cb0027819f7e298dc6c433ba6c4242
patrickgrey <websemantic@yahoo.co.uk>
2022-10-26 13:38:00 +0100

    Got slider ping pong working

--- website-source/ad/main.js
+++ website-source/ad/main.js
@@ -1,4 +1,22 @@
 const wbSlide1 = document.querySelector("#wbSlide1");
 const wbSlide2 = document.querySelector("#wbSlide2");
-let listChangeTime = 500;
-let detailChangeTime = 500;
+let listChangeTime = 3000;
+let detailChangeTime = 3000;
+
+function startGoToDetail() {
+  setTimeout(function () {
+    wbSlide1.style.left = "-100%";
+    wbSlide2.style.left = "0%";
+    startGoToList();
+  }, listChangeTime)
+}
+
+function startGoToList() {
+  setTimeout(function () {
+    wbSlide1.style.left = "0%";
+    wbSlide2.style.left = "100%";
+    startGoToDetail();
+  }, detailChangeTime)
+}
+
+// startGoToDetail();
