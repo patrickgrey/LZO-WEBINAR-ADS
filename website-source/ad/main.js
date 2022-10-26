989bcd2e7367fbf0249affa218a6e5a98f6812e0
patrickgrey <websemantic@yahoo.co.uk>
2022-10-26 15:32:12 +0100

    List prototype done

--- website-source/ad/main.js
+++ website-source/ad/main.js
@@ -1,6 +1,6 @@
 const wbSlide1 = document.querySelector("#wbSlide1");
 const wbSlide2 = document.querySelector("#wbSlide2");
-let listChangeTime = 3000;
+let listChangeTime = 1000;
 let detailChangeTime = listChangeTime;
 
 function startGoToDetail() {
