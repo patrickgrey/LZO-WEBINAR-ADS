f39f02b58a733b37b9525a75cae4a3cbd5546d43
patrickgrey <websemantic@yahoo.co.uk>
2022-10-26 14:22:58 +0100

    Got title and footer in

--- website-source/ad/main.js
+++ website-source/ad/main.js
@@ -1,7 +1,7 @@
 const wbSlide1 = document.querySelector("#wbSlide1");
 const wbSlide2 = document.querySelector("#wbSlide2");
 let listChangeTime = 3000;
-let detailChangeTime = 3000;
+let detailChangeTime = listChangeTime;
 
 function startGoToDetail() {
   setTimeout(function () {
